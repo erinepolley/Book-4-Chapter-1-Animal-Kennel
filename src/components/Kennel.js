0366f3fbb2642cecf7f1fe760c41acf67bc5efdb
Erin Polley <erinepolley@gmail.com>
2019-11-18 14:20:08 -0600

    before normal puppy site

--- src/components/Kennel.js
+++ src/components/Kennel.js
@@ -4,11 +4,16 @@ import AnimalCard from './animal/AnimalCard'
 import LocationCard from './businessInfo/LocationCard'
 import EmployeeCard from './businessInfo/EmployeeCard'
 import OwnerCard from './businessInfo/OwnerCard'
+import NavBar from './nav/NavBar'
+
 
 class Kennel extends Component {
     render() {
         return (
             <div>
+                <div>
+                    <NavBar />
+                </div>
                 <div>
                     <h2>Student Kennels<br />
                         <small>Loving care when you're not there.</small>
